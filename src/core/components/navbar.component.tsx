09d0b283030411cafce7e673a1de74493ec05841
rival <amablerival@gmail.com>
2024-03-10 14:22:36 -0600

    New feather icon library added

--- src/core/components/navbar.component.tsx
+++ src/core/components/navbar.component.tsx
@@ -1,30 +1,6 @@
-import * as Unicons from '@iconscout/react-unicons'
-import { Link } from '@tanstack/react-router'
+import * as Icon from 'react-feather'
 
-import NavbarService from '../services/navbar.service'
-
-const LinkBar = () => {
-  const service = new NavbarService()
-  return (
-    <>
-      <div className="flex items-center justify-center py-6">
-        {service.getNavbarConfig().map((item) => (
-          <div
-            key={item.id}
-            className="mx-5 flex items-center justify-start text-gray"
-          >
-            <Link
-              to={item?.link ? item.link : '/'}
-              activeProps={{ className: 'text-white' }}
-            >
-              {item.title}
-            </Link>
-          </div>
-        ))}
-      </div>
-    </>
-  )
-}
+import LinkBar from './linkbar.component'
 
 const NavBar = ({ title }: { title: string }) => {
   const isDesktop = true
@@ -32,7 +8,7 @@ const NavBar = ({ title }: { title: string }) => {
     <div className="basis-1/12 bg-black">
       <div className="flex h-full w-full items-center p-5 text-white">
         <div className="flex basis-1/2 justify-start text-orange">
-          <Unicons.UilCircle />
+          <Icon.Circle />
         </div>
         <div className="flex basis-1/4 justify-center">
           <h1 className="text-center text-3xl capitalize">{title}</h1>
@@ -42,7 +18,7 @@ const NavBar = ({ title }: { title: string }) => {
             <LinkBar></LinkBar>
           ) : (
             <button className="rounded-lg px-4 py-2">
-              <Unicons.UilEllipsisH />
+              <Icon.Menu />
             </button>
           )}
         </div>
