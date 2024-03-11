c65580ff99326fa482d2a0d359fac7d2c162fdb9
rival <amablerival@gmail.com>
2024-03-10 21:14:19 -0600

    base path ./

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import viteSvgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     eslintPlugin(),
