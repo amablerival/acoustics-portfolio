fe2daca7afa9303da3285d25fecc749b97bbf4b7
rival <amablerival@gmail.com>
2024-03-12 21:13:59 -0600

    Updates

---
+++ assets/home.lazy-C3eX958q.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-CVwlygBK.js";import{H as e}from"./home.component-B1GUpe0D.js";import"./options.component-YBjGhpTr.js";const p=o("/home")({component:e});export{p as Route};
---
+++ assets/index.lazy-D8ihojvF.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-CVwlygBK.js";import{H as t}from"./home.component-B1GUpe0D.js";import"./options.component-YBjGhpTr.js";const p=o("/")({component:t});export{p as Route};
