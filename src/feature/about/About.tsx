95ca598657b44b4fd2ddc1daff05092d72aef139
amablerival <152933991+amablerival@users.noreply.github.com>
2024-06-14 18:06:43 -0600

    About formatted and type removed

--- src/feature/about/About.tsx
+++ src/feature/about/About.tsx
@@ -1,11 +1,11 @@
-import Container from '@/shared/components/Container'
-import Header from '@/shared/components/Header'
-import Options from '@/shared/components/Options'
+import Container from '@/shared/components/Container';
+import Header from '@/shared/components/Header';
+import Options from '@/shared/components/Options';
 
-import AboutService from './about.service'
+import AboutService from './about.service';
 
 const About = () => {
-  const service = new AboutService()
+  const service = new AboutService();
   return (
     <div className="flex size-full flex-col items-center text-white">
       <Container data={{ styleClasses: 'basis-1/4 bg-eggshell' }}>
@@ -16,10 +16,10 @@ const About = () => {
           styleClasses: 'basis-1/6 flex-col bg-white text-orange py-5'
         }}
       >
-        <Header item={service.getBottomData()}></Header>p
+        <Header item={service.getBottomData()}></Header>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
